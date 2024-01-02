f8b6e73ff1b13c79542e206226a733529109cd2f
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2024-01-02 11:57:23 +0000

    DPR2-300 WIP un-hardcode path

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/dprdpdsservice/service/DefinitionsService.kt
@@ -0,0 +1,44 @@
+package uk.gov.justice.digital.hmpps.dprdpdsservice.service
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.*
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.model.FilterType
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.model.ParameterType
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.model.ProductDefinition
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.model.policyengine.Effect
+import uk.gov.justice.digital.hmpps.digitalprisonreportinglib.data.model.policyengine.PolicyType
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.time.LocalDateTime
+import kotlin.io.path.name
+
+@Service
+class DefinitionsService() {
+
+  fun getDefinitions(path: String): List<ProductDefinition> {
+   val gson : Gson = GsonBuilder()
+      .registerTypeAdapter(LocalDateTime::class.java, IsoLocalDateTimeTypeAdaptor())
+      .registerTypeAdapter(FilterType::class.java, FilterTypeDeserializer())
+      .registerTypeAdapter(ParameterType::class.java, SchemaFieldTypeDeserializer())
+      .registerTypeAdapter(Effect::class.java, RuleEffectTypeDeserializer())
+      .registerTypeAdapter(PolicyType::class.java, PolicyTypeDeserializer())
+      .create()
+    val pathURI = this::class.java.classLoader.getResource(path)?.toURI()
+    val dirs = Files.walk(Paths.get(pathURI), 1)
+      .filter { Files.isRegularFile(it) }
+      .filter { item -> item.toString().endsWith(".json")  }
+      .toList()
+    val result = dirs.map<Path, ProductDefinition> {
+      gson.fromJson(
+        this::class.java.classLoader.getResource("$path/${it.name}")?.readText(),
+        object : TypeToken<ProductDefinition>() {}.type
+      )
+    }
+    return result
+  }
+
+}
