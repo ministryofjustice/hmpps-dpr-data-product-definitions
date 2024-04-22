14103265bfd716b88f67949bf9049b8d8c51bb6e
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2024-04-22 08:46:28 +0100

    [DPR2-000] Fix insecure versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   idea
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.+"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.6"
   kotlin("jvm") version "1.9.21"
   kotlin("plugin.spring") version "1.9.21"
   id("jacoco")
