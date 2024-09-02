09a2b975081c8df12210f8bfd0ddb131e5f16c43
Gavriil <135604838+gavriil-g-moj@users.noreply.github.com>
2024-09-02 12:04:44 +0100

    Dpr2 1106 separate folder per env (#153)
    * DPR2-1106 config to have env directories and introduced dev dir
    
    * DPR2-1106 added remaining env directories and config and fix test
    
    * DPR2-1106 update from main
    
    * DPR2-1106 linting
    
    * DPR2-1106 put back original resources
    
    * DPR2-1106 remove original definition resources

--- src/test/kotlin/uk/gov/justice/digital/hmpps/dprdpdsservice/controller/integration/SchemaTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/dprdpdsservice/controller/integration/SchemaTest.kt
@@ -6,6 +6,7 @@ import com.github.fge.jsonschema.main.JsonSchemaFactory
 import org.junit.jupiter.api.Test
 import java.net.URL
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
 import kotlin.io.path.name
 
@@ -16,12 +17,20 @@ class SchemaTest {
   fun `Definitions adhere to schema`() {
     val schemaNode = JsonLoader.fromURL(URL(schemaLocation))
     val schema: JsonSchema = JsonSchemaFactory.byDefault().getJsonSchema(schemaNode)
+    listOf("dev", "test", "preprod", "prod").forEach { env ->
+      val pathURI = this::class.java.classLoader.getResource("$env/definitions/prisons")!!.toURI()
+      val dirs = Files.walk(Paths.get(pathURI), 2)
+        .filter { Files.isRegularFile(it) }
+        .filter { item -> item.toString().endsWith(".json") }
+        .toList()
+      validateJsonFiles(dirs, schema)
+    }
+  }
 
-    val pathURI = this::class.java.classLoader.getResource("definitions/prisons")!!.toURI()
-    val dirs = Files.walk(Paths.get(pathURI), 2)
-      .filter { Files.isRegularFile(it) }
-      .filter { item -> item.toString().endsWith(".json") }
-      .toList()
+  private fun validateJsonFiles(
+    dirs: MutableList<Path>,
+    schema: JsonSchema,
+  ) {
     dirs.forEach {
       println("${it.parent.name}/${it.name}")
 
