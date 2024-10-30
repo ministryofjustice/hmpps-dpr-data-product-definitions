adce582babb2c557aa43cecd6024bf07e66a5942
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2024-10-30 14:31:15 +0000

    [DPR2-1355] Linting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/dprdpdsservice/security/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/dprdpdsservice/security/ResourceServerConfiguration.kt
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 
