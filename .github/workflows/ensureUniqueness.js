4aad407c827c2de07c38ba0b88ad20a88f9dfc45
James Reed <james.reed1@justice.gov.uk>
2026-01-09 12:24:56 +0000

    check variantid uniqueness, make two unique just to check script works

---
+++ .github/workflows/ensureUniqueness.js
@@ -0,0 +1,37 @@
+/**
+ * Detects duplicate variant IDs inside all DPDs
+ */
+
+const fs = require('fs')
+const path = require('path')
+
+const folders = ['dev', 'test', 'preprod', 'prod'].map(env => `dpd/${env}/definitions/prisons/orphanage`)
+const fileMap = {}
+folders.forEach((folder) => {
+  fs.globSync(path.join(process.cwd(), folder, "*.json")).forEach((dpdFileLocation) => {
+    try {
+      const fileContents = JSON.parse(String(fs.readFileSync(dpdFileLocation)))
+      const reportMap = {}
+      fileContents.report.forEach((report) => {
+        if (reportMap[report.id]) {
+          reportMap[report.id] += 1
+          return
+        }
+        reportMap[report.id] = 1
+      })
+      const filteredReportMap = Object.entries(reportMap).filter(([_, timesFound]) => timesFound > 1)
+      if (filteredReportMap.length > 0) {
+        fileMap[dpdFileLocation] = Object.fromEntries(filteredReportMap)
+      }
+    } catch (e) {
+      console.error(`Skipped ${dpdFileLocation} due to error`)
+      console.error(e)
+    }
+  })
+})
+
+if (Object.keys(fileMap).length > 0) {
+  console.error(JSON.stringify(fileMap, undefined, 2))
+  process.exit(1)
+}
+process.exit(0)
