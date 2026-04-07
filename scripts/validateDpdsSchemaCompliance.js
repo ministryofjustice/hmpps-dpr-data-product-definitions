e2c66f03447b255963bc561e2937c97800350bd6
Gavriil <135604838+gavriil-g-moj@users.noreply.github.com>
2026-04-07 20:21:34 +0100

    Dpr2 2721 remove flagged GitHub actions (#492)
    * DPR2-2721 removed flagged github actions and updated the workflows
    
    * DPR2-2721 git ignore node_modules

---
+++ scripts/validateDpdsSchemaCompliance.js
@@ -0,0 +1,29 @@
+/**
+ * Validates all the DPD JSON files against the JSON schema:
+ * https://raw.githubusercontent.com/ministryofjustice/hmpps-digital-prison-reporting-data-product-definitions-schema/main/schema/data-product-definition-schema.json
+ */
+
+
+const Ajv = require("ajv");
+const fs = require("fs");
+const glob = require("glob");
+
+const schema = JSON.parse(fs.readFileSync(process.env.SCHEMA_LOCATION));
+
+const ajv = new Ajv({ allErrors: true });
+const validate = ajv.compile(schema);
+
+const files = glob.sync("dpd/**/*.json");
+
+let valid = true;
+
+for (const file of files) {
+    const data = JSON.parse(fs.readFileSync(file));
+    if (!validate(data)) {
+        console.error(`Errors in ${file}:`);
+        console.error(JSON.stringify(validate.errors, null, 2));
+        valid = false;
+    }
+}
+
+if (!valid) process.exit(1);
