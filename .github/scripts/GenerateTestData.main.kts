69c2ca9a6789c5f19155e7fcbcd622683b44a83b
madhuri.mahesh <madhuri.mahesh@justice.gov.uk>
2026-08-26 16:55:04 +0100

    DPR2-2833- test the failure of the script

--- .github/scripts/GenerateTestData.main.kts
+++ .github/scripts/GenerateTestData.main.kts
@@ -202,6 +202,7 @@ fun sqlScriptGeneration( tableToColumnsMap: Map<String, Map<String, String>>): S
 
     // Build sqlScript for all the DPDs
     val sqlScript = buildString {
+        appendLine("1")
         appendLine("BEGIN; ")
 
         tableToColumnsMap.forEach { (tableName, redshiftColumns) ->
